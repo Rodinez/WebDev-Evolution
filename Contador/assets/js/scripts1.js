5c8ec0e1fb32c589b63af29ae407b622df0ad61c
Gabriel Andrade <106983846+Rodinez@users.noreply.github.com>
2023-03-27 14:41:13 -0300

    Add files via upload

---
+++ Contador/assets/js/scripts1.js
@@ -0,0 +1,17 @@
+let currentNumberWrapper = document.getElementById('currentNumber')
+let currentNumber = 0;
+
+function increment() {
+	currentNumber = currentNumber + 1;
+	currentNumberWrapper.innerHTML = currentNumber;
+	if (currentNumber > -1) {
+		document.getElementById('currentNumber').style.color = 'black';
+	}
+}
+function decrement() {
+	currentNumber = currentNumber - 1;
+	currentNumberWrapper.innerHTML = currentNumber;
+	if (currentNumber < 0) {
+		document.getElementById('currentNumber').style.color = 'red';
+	}
+}
